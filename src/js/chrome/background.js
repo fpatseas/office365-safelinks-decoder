1693507dea536ee3fd3bd86c9404ebe800ccc961
Fotis Patseas <fpatseas@users.noreply.github.com>
2020-11-05 15:02:03 +0200

    Initial commit

---
+++ src/js/chrome/background.js
@@ -0,0 +1,7 @@
+chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+		chrome.tabs.sendMessage(tabs[0].id, {type: 'history-state-updated'});
+	});
+}, {
+	url: [ {hostSuffix: "office.com"} ]
+});
---
+++ src/js/content.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const SafelinksDecoder = {
+	
+	firefox: typeof self !== 'undefined' && typeof self.port !== 'undefined',
+	
+	init() {
+		SafelinksDecoder.registerEventHandlers();
+	},
+	
+	registerEventHandlers() {
+		if (SafelinksDecoder.firefox) {
+			self.port.on('history-state-updated', SafelinksDecoder.processLinks);
+		} else if (typeof chrome !== 'undefined') {
+			chrome.runtime.onMessage.addListener((request) => {
+				if (request.type === 'history-state-updated') {
+					SafelinksDecoder.processLinks();
+				}
+			});
+		} else if (typeof safari !== 'undefined') {
+			safari.self.addEventListener('message', (message) => {
+				if (message.name === 'history-state-updated') {
+					SafelinksDecoder.processLinks();
+				}
+			}, false);
+		}
+
+		window.onload = (event) => SafelinksDecoder.processLinks();
+	},
+
+	processLinks() {
+		let safeLinks = document.querySelectorAll('a[href*="safelinks.protection.outlook.com"]:not([safelink-decoded])');
+	
+		safeLinks.forEach((link, index) => {
+			var url = link.getAttribute("href"),
+				matches = url.match(/(?<=\?url\=).*(?=\&amp\;data\=)/);
+				
+			if( matches ) {
+				url = decodeURIComponent(matches[0]);
+				
+				link.textContent = url;
+				link.setAttribute("safelink-decoded", "true");
+			}
+		});
+	}
+};
+
+SafelinksDecoder.init();
